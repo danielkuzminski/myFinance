967e20ced144f8b8b4e500a384b55b0b0bde7476
danielkuzminski <dankuzmina@gmail.com>
2023-04-22 12:55:24 +0200

    create auth context w/ context provider component

---
+++ src/context/AuthContext.js
@@ -0,0 +1,25 @@
+import { useContext } from "react"
+import { createContext, useReducer } from "react"
+
+export const AuthContext = createContext()
+
+export const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return {...state, user: action.payload}
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+export const AuthContextProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(authReducer, { user: null })
+
+	return (
+        <AuthContext.Provider value={{...state, dispatch}}>
+            {children}
+        </AuthContext.Provider>)
+}
